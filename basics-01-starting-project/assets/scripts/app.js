37e9343af390b1146b2050e134a3163bb8cb2ee0
MaxSuslov <maxsusmail@gmail.com>
2022-10-19 21:49:53 +0200

    Outsorced getting user number input to a function

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,15 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
 
 function add() {
-  const enteredNumber = parseInt(userInput.value);
+  const enteredNumber = getUserNumberInput();
   const calcDescription = `${currentResult} + ${enteredNumber}`;
-  currentResult = currentResult + enteredNumber);
+  currentResult = currentResult + enteredNumber;
   outputResult(currentResult, calcDescription);
 }
 
-// Hey, JS, when the button is clicked, have a look at the button of this name (add)
-// just the function name without () or ""
 addBtn.addEventListener('click', add);
