a40f9cb8f24914765738787425322aa99cf8e0cd
MaxSuslov <maxsusmail@gmail.com>
2022-10-19 21:38:58 +0200

    Outputting the last calculation

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,13 +2,9 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-  // We take 2 global variables (defined in vendor.js) and use them in calculation
-  // .value gives us what the user entered AS A STRING!
-  // .parseInt() converts a string to a number (if applicable)
+  const calcDescription = `${currentResult} + ${userInput.value}`;
   currentResult = currentResult + parseInt(userInput.value);
-  // shorter way to convert a string to a number
-  currentResult = currentResult + +userInput.value;
-  outputResult(currentResult, userInput.value);
+  outputResult(currentResult, calcDescription);
 }
 
 // Hey, JS, when the button is clicked, have a look at the button of this name (add)
