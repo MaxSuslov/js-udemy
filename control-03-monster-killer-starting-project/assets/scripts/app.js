2db40946bab73370a6b2b3a06768a02643bf3241
MaxSuslov <maxsusmail@gmail.com>
2022-10-25 17:10:45 +0200

    Basic attack function with if statements (monster lost/won/draw)

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,6 +1,7 @@
 // add constants which won't change at the beggining - it's a good practise
 // for global variables which we define (hardcode) as a developer use UPPER_CASE - good practise
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -11,6 +12,16 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('You won!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You lost!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert('You have a draw!');
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
