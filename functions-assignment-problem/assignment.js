2a3d668cdb32e1d6ab8f628e335c7ac59367f80f
MaxSuslov <maxsusmail@gmail.com>
2022-11-08 19:19:38 +0100

    Functions assignment(4) done

--- functions-assignment-problem/assignment.js
+++ functions-assignment-problem/assignment.js
@@ -1,5 +1,32 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
-}
+// Original
+// function sayHello(name) {
+//   console.log('Hi ' + name);
+// }
 
-sayHello();
+// sayHello();
+
+// 1 - Re-write the function as an arrow function
+const sayHello = (name) => console.log('Hi', name);
+sayHello('Max');
+
+//2 - Use 3 different syntaxes
+// a) with two arguments (incl. a phrase that replaces "Hi")
+let greeting = 'Hi';
+let personName = 'Max';
+const sayHello2 = (greet, person) => {
+  console.log(greet + ' ' + person);
+};
+sayHello2(greeting, personName);
+
+// b) with no arguments (hard-coded values in function body)
+let sayHello3 = () => console.log('Hi Max');
+sayHello3();
+
+//c) with one returned value (instead of outputting text inside of the function directly).
+let greeting1 = 'Hi';
+let personName1 = 'Max';
+const sayHello4 = (greet, person) => {
+  console.log(greet, person);
+  alert(greet + ' ' + person);
+};
+sayHello4(greeting1, personName1);
